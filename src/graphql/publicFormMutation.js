bb03850deb443208ce394514ef3663a68bbb414b
Bruno Vercelino da Hora <brunodahora@gmail.com>
2019-11-14 08:29:51 -0300

    Fixing submit by adding the submitter email

--- src/graphql/publicFormMutation.js
+++ src/graphql/publicFormMutation.js
@@ -3,7 +3,11 @@ import { gql } from 'apollo-boost';
 export default gql`
   mutation submitPublicForm($filledFields: [FilledField]!) {
     submitPublicForm(
-      input: { formId: "1lf_E0x4", filledFields: $filledFields }
+      input: {
+        formId: "1lf_E0x4"
+        filledFields: $filledFields
+        publicFormSubmitterEmail: "test@test.com"
+      }
     ) {
       repoItem {
         id
